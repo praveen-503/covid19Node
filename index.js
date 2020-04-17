55da5d84693d4bb9937d02f1ddf6b59adbfa6668
chandrasekhara <chandrasekhara@proarch.com>
2020-04-17 17:39:06 +0530

    /hotspotDistance

--- index.js
+++ index.js
@@ -167,6 +167,70 @@ app.get('/hospitalData/:code', async function (req, res) {
     });
 });
 
+app.post('/hotspotDistance', async function (req, res) {
+  var latitude = req.body.latitude;
+  var longitude = req.body.longitude;
+  await request('https://covid19proarch.blob.core.windows.net/datasets/Telangana%20LatLong.xlsx',
+    { encoding: null }, async function (error, response, body) {
+      var workbook = await XLSX.read(body);
+      const wsname = workbook.SheetNames[0];
+      const ws = workbook.Sheets[wsname];
+      var data = await ConvertHotSpotData(XLSX.utils.sheet_to_json(ws, { header: 1 }));
+      var hotspotWithDistance = [];
+      await data.forEach(ele => {
+        var d = getDistanceFromLatLonInKm(latitude, longitude, ele.Latitude, ele.Longitude);
+        hotspotWithDistance.push({
+          data: ele,
+          distance: d
+        })
+      });
+      var result = await GetSortestDistance(Object.assign([], hotspotWithDistance))
+      res.json(result);
+    });
+});
+
+async function GetSortestDistance(data) {
+  await data.sort(function (a, b) {
+    return a.distance - b.distance;
+  });
+  return data.slice(0, 1);
+}
+async function ConvertHotSpotData(data) {
+  var hotspotData = [];
+  await data.forEach(ele => {
+    if (ele[0] != null && ele[0] != "S.No") {
+      hotspotData.push({
+        area: ele[1],
+        city_district: ele[2],
+        State: ele[3],
+        fullAddress: ele[4],
+        Latitude: ele[5],
+        Longitude: ele[6]
+
+      });
+    }
+  })
+  return hotspotData;
+}
+//Haversine formula(it is straight line distance)
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+  var dLon = deg2rad(lon2 - lon1);
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2)
+    ;
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI / 180)
+}
+
 async function ConvertHospitalData(data, code) {
   var index = data[0].indexOf(code);
   var tableData = [];

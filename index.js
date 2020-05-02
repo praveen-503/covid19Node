46251223185867625dc67e40fc47292ba7fcb4a1
chandrasekhara <chandrasekhara@proarch.com>
2020-05-02 11:47:59 +0530

    added pm2 and error handling

--- index.js
+++ index.js
@@ -20,137 +20,106 @@ app.use(function (req, res, next) {
 // Server any static files
 app.use(express.static(path.join(__dirname, 'build')));
 
-
-app.get('/india-latest', async (req, res) => {
-  await request('https://covid19proarch.blob.core.windows.net/datasets/India_Latest.xlsx',
-    { encoding: null }, async function (error, response, body) {
-      var workbook = await XLSX.read(body);
-      const wsname = workbook.SheetNames[0];
-      const ws = workbook.Sheets[wsname];
-      res.json(await ConvertLatestData(XLSX.utils.sheet_to_json(ws, { header: 1 })));
-    });
-
-})
-
-app.get('/india-predict-data', async (req, res) => {
-  await request('https://covid19proarch.blob.core.windows.net/datasets/India_Prediction.xlsx',
-    { encoding: null }, async function (error, response, body) {
-      var workbook = await XLSX.read(body);
-      const wsname = workbook.SheetNames[0];
-      const ws = workbook.Sheets[wsname];
-      res.json(await ConvertPredectionData(XLSX.utils.sheet_to_json(ws, { header: 1 })));
-    });
-})
-
-app.get('/hospitalList', async (req, res) => {
-  await request('https://covid19proarch.blob.core.windows.net/datasets/India%20-%20hospital%2C%20testing%20data.xlsx',
-    { encoding: null }, async function (error, response, body) {
-      var workbook = await XLSX.read(body);
-      const wsname = workbook.SheetNames[0];
-      const ws = workbook.Sheets[wsname];
-      res.json(await ConvertHospitalTestData(XLSX.utils.sheet_to_json(ws, { header: 1 })));
-    });
-})
-
-app.get('/india-actual-daywise', async (req, res) => {
-  await request('https://covid19proarch.blob.core.windows.net/datasets/Inida_Actuals_Daywise.xlsx',
-    { encoding: null }, async function (error, response, body) {
-      var workbook = await XLSX.read(body);
-      const wsname = workbook.SheetNames[0];
-      const ws = workbook.Sheets[wsname];
-      res.json(await ConvertActualDateWiseData(XLSX.utils.sheet_to_json(ws, { header: 1 })));
-    });
-})
-
-app.get('/india-statewise-data', async (req, res) => {
-  await request('https://covid19proarch.blob.core.windows.net/datasets/State_Wise_Actuals.xlsx',
-    { encoding: null }, async function (error, response, body) {
-      var workbook = await XLSX.read(body);
-      const wsname = workbook.SheetNames[0];
-      const ws = workbook.Sheets[wsname];
-      res.json(await ConvertStateWiseData(XLSX.utils.sheet_to_json(ws, { header: 1 })));
-    });
-})
-
 app.get('/indiastatewisedata', async (req, res) => {
-  await request('https://covid19proarch.blob.core.windows.net/datasets/State_Actuals_Daywise.xlsx',
-    { encoding: null }, async function (error, response, body) {
-      var workbook = await XLSX.read(body);
-      const wsname = workbook.SheetNames[0];
-      const ws = workbook.Sheets[wsname];
-      await request('https://covid19proarch.blob.core.windows.net/datasets/State%20Code.xlsx',
-        { encoding: null }, async function (error, response, body1) {
-          var workbook1 = await XLSX.read(body1);
-          const wsname1 = workbook1.SheetNames[0];
-          const ws1 = workbook1.Sheets[wsname1];
-          var stateCode = await GetStateCodeName(XLSX.utils.sheet_to_json(ws1, { header: 1 }));
-          var stateWiseData = await ConvertStateData(XLSX.utils.sheet_to_json(ws, { header: 1 }), stateCode);
+  try {
+    await request('https://covid19proarch.blob.core.windows.net/datasets/State_Actuals_Daywise.xlsx',
+      { encoding: null }, async function (error, response, body) {
+        var workbook = await XLSX.read(body);
+        const wsname = workbook.SheetNames[0];
+        const ws = workbook.Sheets[wsname];
+        await request('https://covid19proarch.blob.core.windows.net/datasets/State%20Code.xlsx',
+          { encoding: null }, async function (error, response, body1) {
+            var workbook1 = await XLSX.read(body1);
+            const wsname1 = workbook1.SheetNames[0];
+            const ws1 = workbook1.Sheets[wsname1];
+            var stateCode = await GetStateCodeName(XLSX.utils.sheet_to_json(ws1, { header: 1 }));
+            var stateWiseData = await ConvertStateData(XLSX.utils.sheet_to_json(ws, { header: 1 }), stateCode);
 
-          var statesData = await TotalOfEachState(Object.assign([], stateWiseData));
-          var top5States = await Top5States(Object.assign([], statesData));
-          res.json({ StatesData: statesData, top5States: top5States });
-        });
+            var statesData = await TotalOfEachState(Object.assign([], stateWiseData));
+            var top5States = await Top5States(Object.assign([], statesData));
+            res.json({ StatesData: statesData, top5States: top5States });
+          });
 
-    });
+      });
+  }
+  catch (e) {
+    res.send(e)
+  }
 })
 
 app.get('/dailyCumulativecharts', async (req, res) => {
-  await request('https://covid19proarch.blob.core.windows.net/datasets/State_Actuals_Daywise.xlsx',
-    { encoding: null }, async function (error, response, body) {
-      var workbook = await XLSX.read(body);
-      const wsname = workbook.SheetNames[0];
-      const ws = workbook.Sheets[wsname];
-      await request('https://covid19proarch.blob.core.windows.net/datasets/State%20Code.xlsx',
-        { encoding: null }, async function (error, response, body1) {
-          var workbook1 = await XLSX.read(body1);
-          const wsname1 = workbook1.SheetNames[0];
-          const ws1 = workbook1.Sheets[wsname1];
-          var stateCode = await GetStateCodeName(XLSX.utils.sheet_to_json(ws1, { header: 1 }));
-          var stateWiseData = await ConvertStateData(XLSX.utils.sheet_to_json(ws, { header: 1 }), stateCode);
+  try {
+    await request('https://covid19proarch.blob.core.windows.net/datasets/State_Actuals_Daywise.xlsx',
+      { encoding: null }, async function (error, response, body) {
+        var workbook = await XLSX.read(body);
+        const wsname = workbook.SheetNames[0];
+        const ws = workbook.Sheets[wsname];
+        await request('https://covid19proarch.blob.core.windows.net/datasets/State%20Code.xlsx',
+          { encoding: null }, async function (error, response, body1) {
+            var workbook1 = await XLSX.read(body1);
+            const wsname1 = workbook1.SheetNames[0];
+            const ws1 = workbook1.Sheets[wsname1];
+            var stateCode = await GetStateCodeName(XLSX.utils.sheet_to_json(ws1, { header: 1 }));
+            var stateWiseData = await ConvertStateData(XLSX.utils.sheet_to_json(ws, { header: 1 }), stateCode);
 
-          res.json(await ConvertToDailyCumlativeChart(Object.assign([], stateWiseData)));
-        });
+            res.json(await ConvertToDailyCumlativeChart(Object.assign([], stateWiseData)));
+          });
 
-    });
+      });
+  }
+  catch (e) {
+    res.send(e)
+  }
 })
 
 app.get('/predectionData/:stateCode', async (req, res) => {
-  await request('https://covid19proarch.blob.core.windows.net/datasets/Prediction_data.xlsx',
-    { encoding: null }, async function (error, response, body) {
-      var workbook = await XLSX.read(body);
-      const wsname = workbook.SheetNames;
-      var stateCode = req.params.stateCode
-      if (wsname.includes(stateCode)) {
-        const ws = workbook.Sheets[stateCode];
-        res.json(await ConvertPredectionDataSheet(XLSX.utils.sheet_to_json(ws, { header: 1 })));
-      }
-      else {
-        res.json("Not Found");
-      }
-    });
+  try {
+    await request('https://covid19proarch.blob.core.windows.net/datasets/Prediction_data.xlsx',
+      { encoding: null }, async function (error, response, body) {
+        var workbook = await XLSX.read(body);
+        const wsname = workbook.SheetNames;
+        var stateCode = req.params.stateCode
+        if (wsname.includes(stateCode)) {
+          const ws = workbook.Sheets[stateCode];
+          res.json(await ConvertPredectionDataSheet(XLSX.utils.sheet_to_json(ws, { header: 1 })));
+        }
+        else {
+          res.json("Not Found");
+        }
+      });
+  }
+  catch (e) {
+    res.send(e)
+  }
 })
 
 app.get('/topDistrictsByStateCode/:stateCode', async function (req, res) {
-  await request('https://covid19proarch.blob.core.windows.net/datasets/india_district_actuals.xlsx',
-    { encoding: null }, async function (error, response, body) {
-      var workbook = await XLSX.read(body);
-      const wsname = workbook.SheetNames[0];
-      const ws = workbook.Sheets[wsname];
-      var stateCode = req.params.stateCode
+  try {
+    await request('https://covid19proarch.blob.core.windows.net/datasets/india_district_actuals.xlsx',
+      { encoding: null }, async function (error, response, body) {
+        var workbook = await XLSX.read(body);
+        const wsname = workbook.SheetNames[0];
+        const ws = workbook.Sheets[wsname];
+        var stateCode = req.params.stateCode
 
-      var totalOfDistrictWise = await TotalDistictsData(XLSX.utils.sheet_to_json(ws, { header: 1 }), stateCode);
-      var top5Districts = await Top5Districts(Object.assign([], totalOfDistrictWise))
-      if (top5Districts.length > 0) {
-        res.json(top5Districts)
-      }
-      else {
-        res.json("Not Found");
-      }
+        var totalOfDistrictWise = await TotalDistictsData(XLSX.utils.sheet_to_json(ws, { header: 1 }), stateCode);
+        var top5Districts = await Top5Districts(Object.assign([], totalOfDistrictWise))
+        if (top5Districts.length > 0) {
+          res.json(top5Districts)
+        }
+        else {
+          res.json("Not Found");
+        }
 
-    });
+      });
+  }
+  catch (e) {
+    res.send(e)
+  }
 });
 
 app.get('/hospitalData/:code', async function (req, res) {
+  try{
   await request('https://covid19proarch.blob.core.windows.net/datasets/India_Others_Details.xlsx',
     { encoding: null }, async function (error, response, body) {
       var workbook = await XLSX.read(body);
@@ -166,9 +135,15 @@ app.get('/hospitalData/:code', async function (req, res) {
         res.json("Not Found");
       }
     });
+  }
+  catch(e)
+  {
+    res.send(e)
+  }
 });
 
 app.post('/hotspotDistance', async function (req, res) {
+  try{
   var latitude = req.body.latitude;
   var longitude = req.body.longitude;
   await request('https://covid19proarch.blob.core.windows.net/datasets/Telangana%20LatLong.xlsx',
@@ -189,6 +164,11 @@ app.post('/hotspotDistance', async function (req, res) {
       var outData = { data: result[0].data, distance: Math.round((result[0].distance + Number.EPSILON) * 100) / 100 }
       res.json(outData);
     });
+  }
+  catch(e)
+  {
+    res.send(e)
+  }
 });
 
 async function GetSortestDistance(data) {
@@ -473,171 +453,6 @@ function NullObjects(data) {
     return 0
   }
   return data
-}
-
-async function ConvertLatestData(data) {
-  var latestData = [];
-  await data.forEach(element => {
-    latestData.push({ name: element[0], totalCount: element[1] })
-  });
-  return latestData;
-}
-
-async function ConvertPredectionData(data) {
-  var p1Series = [];
-  var p2Series = [];
-  var p3Series = [];
-  var p4Series = [];
-  var s1Series = [];
-  var s2Series = [];
-  var s3Series = [];
-  var s4Series = [];
-  var h1Series = [];
-  var h2Series = [];
-  var h3Series = [];
-  var h4Series = [];
-  await data.forEach(element => {
-    if (element[0] != "Country") {
-      var date = new Date(1899, 12, element[2] - 1).toLocaleDateString();
-      p1Series.push({
-        "name": date,
-        "value": element[3]
-      });
-      p2Series.push({
-        "name": date,
-        "value": element[4]
-      });
-      p3Series.push({
-        "name": date,
-        "value": element[5]
-      });
-      p4Series.push({
-        "name": date,
-        "value": element[6]
-      });
-      s1Series.push({
-        "name": date,
-        "value": element[7]
-      });
-      s2Series.push({
-        "name": date,
-        "value": element[8]
-      });
-      s3Series.push({
-        "name": date,
-        "value": element[9]
-      });
-      s4Series.push({
-        "name": date,
-        "value": element[10]
-      });
-      h1Series.push({
-        "name": date,
-        "value": element[11]
-      });
-      h2Series.push({
-        "name": date,
-        "value": element[12]
-      });
-      h3Series.push({
-        "name": date,
-        "value": element[13]
-      });
-      h4Series.push({
-        "name": date,
-        "value": element[14]
-      });
-    }
-  });
-  return {
-    series1: [{ "name": "P1", "series": p1Series }, { "name": "S1", "series": s1Series }],
-    series2: [{ "name": "P2", "series": p2Series }, { "name": "S2", "series": s2Series }],
-    series3: [{ "name": "P3", "series": p3Series }, { "name": "S3", "series": s3Series }],
-    series4: [{ "name": "P4", "series": p4Series }, { "name": "S4", "series": s4Series }],
-    hospitalSeries: [{ "name": "H1", "series": h1Series }, { "name": "H2", "series": h2Series },
-    { "name": "H3", "series": h3Series }, { "name": "H4", "series": h4Series }]
-  }
-
-}
-
-async function ConvertActualDateWiseData(data) {
-
-  var dailyConfirmedCasesSeries = [];
-  var dailyRecoveredCasesSeries = [];
-  var dailyDeceasedCasesSeries = [];
-  var cumlativeConfirmedCasesSeries = [];
-  var cumlativeRecoveredCasesSeries = [];
-  var cumlativeDeceasedCasesSeries = [];
-
-  await data.forEach(element => {
-
-    if (element[0] != "Date" && element[0] != null) {
-      dailyConfirmedCasesSeries.push({
-        "name": element[0],
-        "value": element[1]
-      })
-      dailyRecoveredCasesSeries.push({
-        "name": element[0],
-        "value": element[2]
-      })
-      dailyDeceasedCasesSeries.push({
-        "name": element[0],
-        "value": element[3]
-      })
-      cumlativeConfirmedCasesSeries.push({
-        "name": element[0],
-        "value": element[4]
-      })
-      cumlativeRecoveredCasesSeries.push({
-        "name": element[0],
-        "value": element[5]
-      })
-      cumlativeDeceasedCasesSeries.push({
-        "name": element[0],
-        "value": element[6]
-      })
-
-    }
-  });
-  return {
-    daily: [
-      [{ "name": "Confirmed cases", "series": dailyConfirmedCasesSeries }],
-      [{ "name": "Recovered cases", "series": dailyRecoveredCasesSeries }],
-      [{ "name": "Deceased cases", "series": dailyDeceasedCasesSeries }],
-    ],
-    cumulative: [
-      [{ "name": "Confirmed cases", "series": cumlativeConfirmedCasesSeries }],
-      [{ "name": "Recovered cases", "series": cumlativeRecoveredCasesSeries }],
-      [{ "name": "Deceased cases", "series": cumlativeDeceasedCasesSeries }]
-    ],
-  }
-}
-async function ConvertHospitalTestData(data) {
-  var hospitalTestData = [];
-  await data.forEach(element => {
-    hospitalTestData.push({ name: element[0], totalCount: element[1] })
-  });
-  return hospitalTestData;
-}
-
-async function ConvertStateWiseData(data) {
-  var stateWiseData = [];
-  await data.forEach(element => {
-    if (element[0] != "State" && element[0] != null) {
-      stateWiseData.push(
-        {
-          state: element[0],
-          confirmed: element[1],
-          active: element[2],
-          recovered: element[3],
-          deceased: element[4]
-        });
-    }
-  });
-  await stateWiseData.sort(function (a, b) {
-    return b.confirmed - a.confirmed;
-  });
-  return stateWiseData.slice(0, 5);
 }
 
 app.get('/', (req, res) => {
